3d422fdcea1b2d8305fe7b63a57ae9c4b55b80ae
Yassine Lamtalaa <ysnlmtl3@gmail.com>
2022-06-21 19:57:00 -0400

    add logout

--- app.py
+++ app.py
@@ -4,20 +4,35 @@ from flask import request, redirect, url_for
 
 app = Flask(__name__)
 
-username = "Stranger"
+username = None
 
 @app.route("/")
 @app.route("/home")
 def get_index():
-    global username
+    if username == None:
+        return redirect (url_for('get_login'))
     return render_template('home.html', name = username)
+
+@app.route("/other")
+def get_other():
+    if username == None:
+        return redirect (url_for('get_login'))
+    return render_template('other.html', name = username)
 
 @app.route("/login", methods=['GET'])
 def get_login():
+    if username != None:
+        return redirect (url_for('get_index'))
     return render_template('login.html')
 
 @app.route("/login", methods=['POST'])
 def post_login():
     global username
-    username = request.form.get("username", "stranger")
+    username = request.form.get("username", None)
     return redirect (url_for('get_index'))
+
+@app.route("/logout", methods=['GET'])
+def get_logout():
+    global username
+    username == None
+    return redirect (url_for('get_login'))
