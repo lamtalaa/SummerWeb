322bbc49359183569d54373e9192e5d0aa6542f0
Yassine Lamtalaa <ysnlmtl3@gmail.com>
2022-06-21 20:35:31 -0400

    logout cookies

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from flask import request, redirect, url_for
+from flask import request, redirect, url_for, make_response
 
 app = Flask(__name__)
 
@@ -9,30 +9,34 @@ username = None
 @app.route("/")
 @app.route("/home")
 def get_index():
+    username = request.cookies.get("username", None)
     if username == None:
         return redirect (url_for('get_login'))
     return render_template('home.html', name = username)
 
 @app.route("/other")
 def get_other():
+    username = request.cookies.get("username", None)
     if username == None:
         return redirect (url_for('get_login'))
     return render_template('other.html', name = username)
 
 @app.route("/login", methods=['GET'])
 def get_login():
+    username = request.cookies.get("username", None)
     if username != None:
         return redirect (url_for('get_index'))
     return render_template('login.html')
 
 @app.route("/login", methods=['POST'])
 def post_login():
-    global username
     username = request.form.get("username", None)
-    return redirect (url_for('get_index'))
+    response = make_response(redirect (url_for('get_index')))
+    response.set_cookie("username", username)
+    return response
 
 @app.route("/logout", methods=['GET'])
 def get_logout():
-    global username
-    username == None
-    return redirect (url_for('get_login'))
+    response = make_response(redirect (url_for('get_login')))
+    response.delete_cookie("username")
+    return response
