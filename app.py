56c4ea81a97ef2f12c9cc2a6687c136d59370dcd
Yassine Lamtalaa <ysnlmtl3@gmail.com>
2022-06-21 21:08:51 -0400

    basic session stuff

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 from flask import Flask
 from flask import render_template
 from flask import request, redirect, url_for, make_response
+from flask import session
 
 app = Flask(__name__)
+app.secret_key = "qwertyuioplkjhgfdsazxcvbnm"
 
 username = None
 
 @app.route("/")
 @app.route("/home")
 def get_index():
-    username = request.cookies.get("username", None)
-    if username == None:
+    if "username" in session:
+        username = session['username']
+    else:
         return redirect (url_for('get_login'))
     return render_template('home.html', name = username)
 
 @app.route("/other")
 def get_other():
-    username = request.cookies.get("username", None)
-    if username == None:
+    if "username" in session:
+        username = session['username']
+    else:
         return redirect (url_for('get_login'))
     return render_template('other.html', name = username)
 
@@ -31,12 +35,13 @@ def get_login():
 @app.route("/login", methods=['POST'])
 def post_login():
     username = request.form.get("username", None)
-    response = make_response(redirect (url_for('get_index')))
-    response.set_cookie("username", username)
-    return response
+    if username != None:
+        session['username'] = username
+        return redirect (url_for('get_index'))
+    else:
+        return redirect (url_for('get_login'))
 
 @app.route("/logout", methods=['GET'])
 def get_logout():
-    response = make_response(redirect (url_for('get_login')))
-    response.delete_cookie("username")
-    return response
+    session.pop('username', None)
+    return redirect (url_for('get_login'))
